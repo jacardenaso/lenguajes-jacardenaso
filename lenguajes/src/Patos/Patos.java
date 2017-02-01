7881da9a23f85d568c486f4451adaa1eaba9a1e0
jr.armando <jr.armando@10.100.150.68>
2017-02-01 13:58:30 -0600

    Patos

---
+++ lenguajes/src/Patos/Patos.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Patos;
+
+/**
+ *
+ * @author jr.armando
+ */
+public class Patos {
+    private String canadiense_cafe;
+   private String europeo_negro;
+   private int mexicano_blanco;
+
+    public String getCanadiense_cafe() {
+        return canadiense_cafe;
+    }
+
+    public void setCanadiense_cafe(String canadiense_cafe) {
+        this.canadiense_cafe = canadiense_cafe;
+    }
+
+    public String getEuropeo_negro() {
+        return europeo_negro;
+    }
+
+    public void setEuropeo_negro(String europeo_negro) {
+        this.europeo_negro = europeo_negro;
+    }
+
+    public int getMexicano_blanco() {
+        return mexicano_blanco;
+    }
+
+    public void setMexicano_blanco(int mexicano_blanco) {
+        this.mexicano_blanco = mexicano_blanco;
+    }
+   
+
+}
